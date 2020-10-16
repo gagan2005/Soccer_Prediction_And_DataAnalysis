99b579900125cc7bf28baff364a41254211ff49c
swapnilnarad2000 <57103911+swapnilnarad2000@users.noreply.github.com>
2020-10-16 16:54:40 +0530

    Added combined match files and script for it

---
+++ MatchData_Combine.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+a = pd.DataFrame()
+b = pd.DataFrame()
+for i in range(1993,2021):
+    print(i)
+    df = pd.read_csv(str(i)+'.csv' , error_bad_lines=False)
+    df['Season']=i
+    b = pd.concat([a,df])
+    a = b
+a.to_csv('MatchData.csv')
+print(a)
+
