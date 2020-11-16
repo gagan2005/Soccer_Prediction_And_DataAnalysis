83a394fe80660f075af3815264c9b9d65707335c
gagan <gagans@iitbhilai.ac.in>
2020-11-16 18:35:25 +0530

    model changes

--- MatchData_Combine.py
+++ MatchData_Combine.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 a = pd.DataFrame()
 b = pd.DataFrame()
-for i in range(1993,2021):
+for i in range(2000,2021):
     print(i)
     df = pd.read_csv('DS250_SeasonData/'+str(i)+'.csv' , error_bad_lines=False)
     df['Season']=i
